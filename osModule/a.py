f13060a22576b36be6861c22ed77da5d91c89346
karth <karthiknaveen22022002@gmail.com>
2021-10-04 16:37:27 +0530

    added pillow codes and os module codes

---
+++ osModule/a.py
@@ -0,0 +1,16 @@
+import os
+# print(dir(os))
+# print(os.getcwd())
+# # cwd means current working directory
+os.chdir("/Users/karth/Desktop")
+# # chdir means change directory
+# print(os.getcwd())
+
+print(os.listdir())
+# it is used to list all the directories present
+# in that dierectory
+
+
+
+
+
